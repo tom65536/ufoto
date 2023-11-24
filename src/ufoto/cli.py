550025f119d7e1f9d3950e758c3e669b5e803060
tom65536 <peregrinus@planet.ms>
2023-11-24 21:04:57 +0100

    [WIP] adding cli.

--- src/ufoto/cli.py
+++ src/ufoto/cli.py
@@ -3,6 +3,8 @@ import click
 
 
 @click.command()
+@click.option('--output', '-o',
+              click.File('wb'),
 def main(format_: ) -> None:
   """Convert UFO model files to a given format."""
   click.echo('xxx')
